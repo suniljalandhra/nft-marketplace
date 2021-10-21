894e93960388437b99b01dd39e6999223dc06717
suniljalandhra <jalandhrasunil99@gmail.com>
2021-10-21 23:27:09 +0530

    test:- added test for adding tokenURI

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,28 @@
+const { getContractAddress } = require("@ethersproject/address");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("NFTMarket", function () {
+  it("Should create and execute market sales", async function () {
+    // deploy marketplace
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    // deploy the nft contract 
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftAddress = nft.address
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    let listingPrice = await market.getListingPrice()
+    ListingPrice = listingPrice.toString()
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const auctionPrice = ethers.utils.parseUnits('100','ether')
+    // added the tokenURI for a nft
+    await nft.createToken("https://www.mytokenlocation.com")
+    await nft.createToken("https://www.mytokenlocation2.com")
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
