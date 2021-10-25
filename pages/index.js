9474a1da1f96740c121d34c3be65d8c9f4d78ba3
suniljalandhra <jalandhrasunil99@gmail.com>
2021-10-25 23:29:13 +0530

    issue:- page should be showing nothing to show yet

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,8 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Model from "web3modal"
 
-import {
-  nftaddress, nftmarketaddress
-} from '../config.js'
+ const nftaddress ="0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+ const nftmarketaddress="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
@@ -54,7 +53,7 @@ export default function Home() {
     await transaction.wait()
     loadNFTs()
   }
-  if (loadingState === 'loaded' && !nfts.length)return(<h1 className="px-20 py-10 text-3xl">No items in marketplace </h1>)
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>)
 
   return (
     <div className="flex justify-center">
