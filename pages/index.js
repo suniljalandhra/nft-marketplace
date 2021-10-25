8e808e39bfc0168e2ae4b13c0b151fa44e5b2116
suniljalandhra <jalandhrasunil99@gmail.com>
2021-10-25 19:09:17 +0530

    added : buynft loadnfts

--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,44 @@ export default function Home() {
     setNfts(items)
     setLoadingSate('loaded')
   }
+  async function buyNft(nft){
+    const web3Modal = new Web3Model()
+    const connection = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(connection)
+
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(nftmarketaddress,Market.abi,signer)
+
+    const price = ethers.utils.parseUnits(nft.price.toString(),'ether')
+    const transaction = await contract.createMarketSale(nftaddress,nft.tokenId,{value:price})
+    await transaction.wait()
+    loadNFTs()
+  }
+  if (loadingState === 'loaded' && !nfts.length)return(<h1 className="px-20 py-10 text-3xl">No items in marketplace </h1>)
+
   return (
-    <div className={styles.container}>
+    <div className="flex justify-center">
+      <div className="px-4" style= {{maxWidth:'1600px'}}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {
+            nfts.map((nft, i) => (
+              <div key={i} className="border shadow rounded-xl overflow-hidden">
+                <img src={nft.image}/>
+                <div className="p-4">
+                  <p style={{height: '64px'}} className="text-2xl font-semibold">{nft.name}</p>
+                  <div style={{height:'70px', overflow: 'hidden'}}>
+                    <p className="text-gray-400">{nft.description}</p>
+                  </div>
+                  </div>
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl mb-4 font-bold text-white">{nft.price}</p>
+                    <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                  </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
       
     </div>
   )
