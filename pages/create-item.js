c0651ae1590b1c3adda6a09ddd9adcb00abf17e8
suniljalandhra <jalandhrasunil99@gmail.com>
2021-10-28 23:26:16 +0530

    ipfs-infura added

--- pages/create-item.js
+++ pages/create-item.js
@@ -2,4 +2,12 @@ import {useState} from "react";
 import {ethers} from 'ethers';
 import {create as ipfsHttpClient }from 'ipfs-http-client';
 import { useRouter } from 'next/router';
-import Web3Model from 'web3modal'
+import Web3Model from 'web3modal';
+
+// this will work when I will deploy the contract on chain 
+// import {
+//     nftaddress, nftmarketaddress
+// } from '../config'
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import NFTMarket from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
